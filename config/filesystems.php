05e8f42529ff03254eeb4c5c400deca9b3822ccf
Anwarul Fattach <emosekolah@gmail.com>
2021-05-27 13:42:46 +0700

    done, ready to deploy

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,24 +32,18 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
-        ],
-
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+        'gcs' => [
+            'driver' => 'gcs',
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'project-id'),
+            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', ''),
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', 'sehatmentalku'),
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null),
         ],
 
     ],
